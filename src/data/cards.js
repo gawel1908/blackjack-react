104a91608b8316511b68e53f4fa8fd9ec3ab2a53
gawalkiewiczg <g.gawalkiewicz@youlead.pl>
2020-01-01 12:51:08 +0100

    some fixes

--- src/data/cards.js
+++ src/data/cards.js
@@ -1,262 +1,123 @@
 const cards = [
-    /*{
-      "cardId": "2c",
-      "cardClass": "cards card-2c",
-      "cardValue": 2
-    },
+    
     {
-      "cardId": "2d",
-      "cardClass": "cards card-2d",
-      "cardValue": 2
-    },
-    {
-      "cardId": "2h",
-      "cardClass": "cards card-2h",
-      "cardValue": 2
-    },
-    {
-      "cardId": "2s",
-      "cardClass": "cards card-2s",
-      "cardValue": 2
-    },
-    {
-      "cardId": "3c",
-      "cardClass": "cards card-3c",
-      "cardValue": 3
-    },
-    {
-      "cardId": "3d",
-      "cardClass": "cards card-3d",
-      "cardValue": 3
-    },
-    {
-      "cardId": "3h",
-      "cardClass": "cards card-3h",
-      "cardValue": 3
-    },
-    {
-      "cardId": "3s",
-      "cardClass": "cards card-3s",
-      "cardValue": 3
-    },
-    {
-      "cardId": "4c",
-      "cardClass": "cards card-4c",
-      "cardValue": 4
-    },
-    {
-      "cardId": "4d",
-      "cardClass": "cards card-4d",
-      "cardValue": 4
-    },
-    {
-      "cardId": "4h",
-      "cardClass": "cards card-4h",
-      "cardValue": 4
-    },
-    {
-      "cardId": "4s",
-      "cardClass": "cards card-4s",
-      "cardValue": 4
-    },
-    {
-      "cardId": "5c",
-      "cardClass": "cards card-5c",
-      "cardValue": 5
-    },
-    {
-      "cardId": "5d",
-      "cardClass": "cards card-5d",
-      "cardValue": 5
-    },
-    {
-      "cardId": "5h",
-      "cardClass": "cards card-5h",
-      "cardValue": 5
-    },
-    {
-      "cardId": "5s",
-      "cardClass": "cards card-5s",
-      "cardValue": 5
-    },
-    {
-      "cardId": "6c",
-      "cardClass": "cards card-6c",
-      "cardValue": 6
-    },
-    {
-      "cardId": "6d",
-      "cardClass": "cards card-6d",
-      "cardValue": 6
-    },
-    {
-      "cardId": "6h",
-      "cardClass": "cards card-6h",
-      "cardValue": 6
-    },
-    {
-      "cardId": "6s",
-      "cardClass": "cards card-6s",
-      "cardValue": 6
-    },
-    {
-      "cardId": "7c",
-      "cardClass": "cards card-7c",
-      "cardValue": 7
-    },
-    {
-      "cardId": "7d",
-      "cardClass": "cards card-7d",
-      "cardValue": 7
-    },
-    {
-      "cardId": "7h",
-      "cardClass": "cards card-7h",
-      "cardValue": 7
-    },
-    {
-      "cardId": "7s",
-      "cardClass": "cards card-7s",
-      "cardValue": 7
-    },
-    {
-      "cardId": "8c",
-      "cardClass": "cards card-8c",
-      "cardValue": 8
-    },
-    {
-      "cardId": "8d",
-      "cardClass": "cards card-8d",
-      "cardValue": 8
-    },
-    {
-      "cardId": "8h",
-      "cardClass": "cards card-8h",
-      "cardValue": 8
-    },
-    {
-      "cardId": "8s",
-      "cardClass": "cards card-8s",
-      "cardValue": 8
-    },*/
-    {
-      "cardId": "9c",
-      "cardClass": "cards card-9c",
+      "cardID": "9C",
+      "cardClass": "cards card-9C",
       "cardValue": 9
     },
     {
-      "cardId": "9d",
-      "cardClass": "cards card-9d",
+      "cardID": "9D",
+      "cardClass": "cards card-9D",
       "cardValue": 9
     },
     {
-      "cardId": "9h",
-      "cardClass": "cards card-9h",
+      "cardID": "9H",
+      "cardClass": "cards card-9H",
       "cardValue": 9
     },
     {
-      "cardId": "9s",
-      "cardClass": "cards card-9s",
+      "cardID": "9S",
+      "cardClass": "cards card-9S",
       "cardValue": 9
     },
     {
-        "cardId": "10c",
-        "cardClass": "cards card-10c",
+        "cardID": "10C",
+        "cardClass": "cards card-10C",
         "cardValue": 10
     },
       {
-        "cardId": "10d",
-        "cardClass": "cards card-10d",
+        "cardID": "10D",
+        "cardClass": "cards card-10D",
         "cardValue": 10
       },
       {
-        "cardId": "10h",
-        "cardClass": "cards card-10h",
+        "cardID": "10H",
+        "cardClass": "cards card-10H",
         "cardValue": 10
       },
       {
-        "cardId": "10s",
-        "cardClass": "cards card-10s",
+        "cardID": "10S",
+        "cardClass": "cards card-10S",
         "cardValue": 10
       },
       {
-        "cardId": "jc",
-        "cardClass": "cards card-jc",
+        "cardID": "JC",
+        "cardClass": "cards card-JC",
         "cardValue": 2
       },
       {
-        "cardId": "jd",
-        "cardClass": "cards card-jd",
+        "cardID": "JD",
+        "cardClass": "cards card-JD",
         "cardValue": 2
       },
       {
-        "cardId": "jh",
-        "cardClass": "cards card-jh",
+        "cardID": "JH",
+        "cardClass": "cards card-JH",
         "cardValue": 2
       },
       {
-        "cardId": "js",
-        "cardClass": "cards card-js",
+        "cardID": "JS",
+        "cardClass": "cards card-JS",
         "cardValue": 2
       },
       {
-        "cardId": "qc",
-        "cardClass": "cards card-qc",
+        "cardID": "QC",
+        "cardClass": "cards card-QC",
         "cardValue": 3
       },
       {
-        "cardId": "qd",
-        "cardClass": "cards card-qd",
+        "cardID": "QD",
+        "cardClass": "cards card-QD",
         "cardValue": 3
       },
       {
-        "cardId": "qh",
-        "cardClass": "cards card-qh",
+        "cardID": "QH",
+        "cardClass": "cards card-QH",
         "cardValue": 3
       },
       {
-        "cardId": "qs",
-        "cardClass": "cards card-qs",
+        "cardID": "QS",
+        "cardClass": "cards card-QS",
         "cardValue": 3
       },
       {
-        "cardId": "kc",
-        "cardClass": "cards card-kc",
+        "cardID": "KC",
+        "cardClass": "cards card-kKC",
         "cardValue": 4
       },
       {
-        "cardId": "kd",
-        "cardClass": "cards card-kd",
+        "cardID": "KD",
+        "cardClass": "cards card-KD",
         "cardValue": 4
       },
       {
-        "cardId": "kh",
-        "cardClass": "cards card-kh",
+        "cardID": "KH",
+        "cardClass": "cards card-KH",
         "cardValue": 4
       },
       {
-        "cardId": "ks",
-        "cardClass": "cards card-ks",
+        "cardID": "KS",
+        "cardClass": "cards card-KS",
         "cardValue": 4
       },
       {
-        "cardId": "ac",
-        "cardClass": "cards card-ac",
+        "cardID": "AC",
+        "cardClass": "cards card-AC",
         "cardValue": 11
       },
       {
-        "cardId": "ad",
-        "cardClass": "cards card-asd",
+        "cardID": "AD",
+        "cardClass": "cards card-AD",
         "cardValue": 11
       },
       {
-        "cardId": "ah",
-        "cardClass": "cards card-ah",
+        "cardID": "AH",
+        "cardClass": "cards card-AH",
         "cardValue": 11
       },
       {
-        "cardId": "as",
-        "cardClass": "cards card-js",
+        "cardID": "AS",
+        "cardClass": "cards card-AS",
         "cardValue": 11
       },
       
